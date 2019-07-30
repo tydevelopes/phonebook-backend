459f6f91e2a44d54805a34d90e4601216249e970
Emmanuel <eadjeifrimpong@worcester.edu>
2019-07-30 12:43:40 -0400

    custom morgan logger

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
+
+morgan.token('data', (request, response) => {
+  return JSON.stringify(request.body);
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
+
 
 let persons = [
   {
