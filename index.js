881e182a44c28915b714da692ae466e5852f6b88
Emmanuel <eadjeifrimpong@worcester.edu>
2019-08-03 20:08:06 -0400

    add error handling

--- index.js
+++ index.js
@@ -53,25 +53,38 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-// get phone stats
-// app.get('/api/info', (request, response) => {
-//   response.send(`
-//     <p>Phonebook has info for ${persons.length} people</p>
-//     <p>${new Date().toString()}</p>
-//   `);
-// });
+//get phone stats
+app.get('/api/info', (request, response) => {
+  Phonebook.estimatedDocumentCount((error, count) => {
+    console.log('num of phonebook:', count);
+    response.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${new Date().toString()}</p>
+    `);
+  });
+});
 
 // get a phonebook
-app.get('/api/persons/:id', (request, response) => {
-  Phonebook.findById(request.params.id).then(person =>
-    response.json(person.toJSON())
-  );
+app.get('/api/persons/:id', (request, response, next) => {
+  Phonebook.findById(request.params.id)
+    .then(person => {
+      console.log(person);
+
+      if (person) {
+        response.json(person.toJSON());
+      } else {
+        response.status(204).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 // delete a phonebook
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id;
-  Phonebook.deleteOne({ _id: id }).then(res => response.status(204).end());
+  Phonebook.deleteOne({ _id: id })
+    .then(res => response.status(204).end())
+    .catch(error => next(error));
 });
 
 // add a phonebook
@@ -85,12 +98,64 @@ app.post('/api/persons', (request, response) => {
   // if (persons.some(person => person.name === body.name)) {
   //   return response.status(400).json({ error: 'name must be unique' });
   // }
-  const person = new Phonebook({
+
+  Phonebook.find({ name: body.name }, (error, docs) => {
+    console.log('error:', error);
+    console.log('docs matching:', docs);
+
+    // update number if name exist
+    if (docs.length !== 0) {
+      const id = docs[0]._id;
+      const phonebook = {
+        name: docs[0].name,
+        number: body.number
+      };
+      Phonebook.findByIdAndUpdate(id, phonebook, { new: true })
+        .then(updatedPerson => {
+          response.json(updatedPerson.toJSON());
+        })
+        .catch(error => next(error));
+    } else {
+      const person = new Phonebook({
+        name: body.name,
+        number: body.number
+      });
+      person.save().then(savedPerson => response.json(savedPerson.toJSON()));
+    }
+  });
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const phonebook = {
     name: body.name,
     number: body.number
-  });
-  person.save().then(savedPerson => response.json(savedPerson.toJSON()));
+  };
+
+  Phonebook.findByIdAndUpdate(request.params.id, phonebook, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON());
+    })
+    .catch(error => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
